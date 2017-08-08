b1bef962475eba4c1a5465122196114413b457e9
seufster <seufster@gmail.com>
2017-08-07 22:36:32 -0400

    init

---
+++ pidragon-pm/src/main/java/org/pidragon/PidragonPmApplication.java
@@ -0,0 +1,12 @@
+package org.pidragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidragonPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidragonPmApplication.class, args);
+	}
+}
